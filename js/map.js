46a4df46084b2225992e6734f7923cb1ace899d5
PenthouseEagle <oorlov@gmail.com>
2017-08-31 18:03:03 +0300

    Check field form

--- js/map.js
+++ js/map.js
@@ -192,3 +192,82 @@ showObjectBookingOnInfoPanel(objectBookings[0]);
 
 var infoPanelButtonClose = document.querySelector('.dialog__close');
 infoPanelButtonClose.addEventListener('click', infoPanelButtonCloseClickHandler);
+
+/*
+ * Tuning input form
+ */
+var fieldTimeIn = document.getElementById('timein');
+var fieldTimeOut = document.getElementById('timeout');
+var fieldType = document.getElementById('type');
+var fieldPrice = document.getElementById('price');
+var fieldRooms = document.getElementById('room_number');
+var fieldCapacity = document.getElementById('capacity');
+
+fieldTimeIn.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value !== fieldTimeOut.value) {
+    fieldTimeOut.value = target.value;
+  }
+});
+
+fieldTimeOut.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value !== fieldTimeIn.value) {
+    fieldTimeIn.value = target.value;
+  }
+});
+
+fieldType.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value === 'flat') {
+    fieldPrice.value = 1000;
+  }
+  if (target.value === 'bungalo') {
+    fieldPrice.value = 0;
+  }
+  if (target.value === 'house') {
+    fieldPrice.value = 5000;
+  }
+  if (target.value === 'palace') {
+    fieldPrice.value = 10000;
+  }
+});
+
+fieldRooms.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value === '1') {
+    fieldCapacity.value = 1;
+  }
+  if (target.value === '2') {
+    fieldCapacity.value = 2;
+  }
+  if (target.value === '3') {
+    fieldCapacity.value = 3;
+  }
+  if (target.value === '100') {
+    fieldCapacity.value = 0;
+  }
+});
+/*
+adInput.addEventListener('invalid', function () {
+  if (!userNameInput.validity.valid) {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    }
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+adInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length === 0) {
+    target.setCustomValidity('');
+  } else {
+    target.setCustomValidity('');
+  }
+}); */
